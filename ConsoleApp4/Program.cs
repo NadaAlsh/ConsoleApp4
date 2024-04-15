33467ff4f76198286140e6ebce9f9e64250bfcb7
83315 <alshaibaninada@gmail.com>
2024-04-15 09:42:40 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp4;
+
+Student student = new Student("Nada", "Computer Engineering", 3.71);
+student.PrintSummary();
+
+
---
+++ ConsoleApp4/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Major { get; set; }
+        public double GPA { get; set; }
+         
+        public Student(string name, string major, double gpa)
+        {
+            Name = name;
+            Major = major;
+            GPA = gpa;
+        }
+
+        public void PrintSummary()
+        {
+            Console.WriteLine($"Student Name: {Name}");
+            Console.WriteLine($"Major: {Major}");
+            Console.WriteLine($"GPA: {GPA:F2}");
+        }
+    }
+}
